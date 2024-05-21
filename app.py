3db884db46855a288e51b5cbfbfb642bf9e0c01f
root <root@ip-172-31-29-190.eu-west-1.compute.internal>
2024-05-21 12:05:42 +0000

    s

--- app.py
+++ app.py
@@ -9,72 +9,71 @@ products = [
         'id': 1,
         'name': 'LED Light Bulb',
         'price': 9.99,
-        'image': 'led-bulb.jpg'
+        'image': 'led-bulb.jpg',
+        'category': 'Washing Machines'  # Corrected category
     },
     {
         'id': 2,
         'name': 'Power Strip',
         'price': 14.99,
-        'image': 'power-strip.jpg'
+        'image': 'power-strip.jpg',
+        'category': 'Washing Machines'  # Corrected category
     },
     {
         'id': 3,
         'name': 'Extension Cord',
         'price': 7.99,
-        'image': 'extension-cord.jpg'
+        'image': 'extension-cord.jpg',
+        'category': 'Washing Machines'  # Corrected category
     },
     {
         'id': 4,
         'name': 'Smart Plug',
         'price': 19.99,
-        'image': 'smart-plug.jpg'
+        'image': 'smart-plug.jpg',
+        'category': 'Fridges'  # Corrected category
     },
     {
         'id': 5,
         'name': 'Surge Protector',
         'price': 24.99,
-        'image': 'surge-protector.jpg'
+        'image': 'surge-protector.jpg',
+        'category': 'Fridges'  # Corrected category
     },
     {
         'id': 6,
         'name': 'Electrical Tape',
         'price': 3.99,
-        'image': 'electrical-tape.jpg'
+        'image': 'electrical-tape.jpg',
+        'category': 'Fridges'  # Added category
     },
     {
         'id': 7,
         'name': 'Cable Ties',
         'price': 5.99,
-        'image': 'cable-ties.jpg'
-
+        'image': 'cable-ties.jpg',
+        'category': 'Ovens'  # Added category
     },
     {
         'id': 8,
         'name': 'Circuit Breaker',
         'price': 39.99,
-        'image': 'circuit-breaker.jpg'
-
+        'image': 'circuit-breaker.jpg',
+        'category': 'Ovens'  # Added category
     }
 ]
 
-# product_categories = {}
-# for product in products:
-#     category = product['ccc']
-#     if category not in product_categories:
-#         product_categories[ccc] = []
-#     product_categories[ccc].append(product)
-    
-    
-    
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query', '').lower()
-    matching_products = [p for p in products if query in p['name'].lower()]
-    return render_template('search.html', query=query, products=matching_products)
+# Group products by category
+product_categories = {}
+for product in products:
+    category = product['category']
+    if category not in product_categories:
+        product_categories[category] = []
+    product_categories[category].append(product)
 
 @app.route('/')
 def index():
-    return render_template('index.html', products=products)
+    return render_template('index.html', product_categories=product_categories)
 
 @app.route('/product/<int:product_id>', methods=['GET', 'POST'])
 def product(product_id):
@@ -90,4 +89,4 @@ def product(product_id):
     return render_template('product.html', product=product)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
